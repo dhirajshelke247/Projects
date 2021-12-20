8414be9abe30cd8f3894bf13db62c273dd73c72b
Dhiraj Shelke <247dhiraj247@gmail.com>
2021-12-20 16:35:02 +0530

    initial commit for TE

---
+++ SPPU/TE/WT/Travel/bottom.php
@@ -0,0 +1,65 @@
+<div class="fotter">	
+	 <div class="container">
+<img src="images/logo1.png" alt="Detox Travels">
+		 <div class="fotter-grids">
+			 <div class="col-md-4 fotter-left">
+			 
+			 <p style="text-align:justify">I am Dhiraj Shelke, your tour director for your next tour. I have been working as a guide for the last five years with americans, australians and spanish groups around the most important places in World. This blog is for me, but sharing it with you, it is also for you, so you can find useful information for your next tour. This is my wish. </p>
+			 </div>
+			 <div class="col-md-4 fotter-middle">
+				 <h3>Questions About Our Tours?</h3>
+				 <div class="footer-list">
+						<ul>
+                        <li style="text-align:justify"><a href="#"><span></span>
+Our Customer Service Department is always happy to answer any questions you might have. Please visit our Contact Page to contact us or call us at 9876543210.</a></li></ul> </div>
+						<h3>Why us ?</h3>
+                        <div class="footer-list">
+                        <ul>
+
+ <li style="text-align:justify"><a href="#"><span></span>Customer Satisfaction<br/></a></li>
+ <li style="text-align:justify"><a href="#"><span></span>Superior customer service, 24x7 Dedicated helpline and over 5 million delighted customers & still growing.<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Best Deals Guaranteed<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Great experiences at lowest prices guaranteed.<br/></a></li>
+</a></li> </ul>
+			 </div>
+             
+			 </div>
+			 <div class="col-md-4 fotter-right" style="padding-left:70px">
+			 <h3>Why buy from us?</h3><br/>
+              <div class="footer-list">
+			  <ul>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Payment Security</a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Travel Guide</a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Social Responsibility</a></li>
+               </ul> </div></div>
+			 <div class="social-icons">
+			 <a href="#"><span class="facebook"> </span></a>
+			 <a href="#"><span class="twitter"> </span></a>
+			 <a href="#"><span class="googleplus"> </span></a>
+			 <a href="#"><span class="pinterest"> </span></a>
+			 <a href="https://www.instagram.com/dtox24_7"><span class="instagram"> </span></a>
+			 </div>
+			 <div class="clearfix"></div>
+	     </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+</div>  
+<div class="copyright text-right">
+<p style="padding-right:100px">Designed by: Dhiraj Shelke</p>
+</div>
+  <script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.nav.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$('#nav').onePageNav({
+			begin: function() {
+			console.log('start')
+			},
+			end: function() {
+			console.log('stop')
+			}
+		});
+	});
+	</script>
+  
